9c55465eba16a329bcc38f21c5ae32e288f3b99e
Tomasz Klim <github@tomaszklim.pl>
2016-11-24 16:46:16 +0100

    option to create new mysql users with given host name instead of localhost

--- mysql/create-database-and-user.sh
+++ mysql/create-database-and-user.sh
@@ -5,8 +5,13 @@
 
 
 
+hstype=`/opt/farm/scripts/config/detect-hostname-type.sh $4`
+
 if [ "$3" = "" ]; then
-	echo "usage: $0 <new-database> <new-login> <new-password>"
+	echo "usage: $0 <new-database> <new-login> <new-password> [hostname]"
+	exit 1
+elif [ "$4" != "" ] && [ "$4" != "%" ] && [ "$hstype" != "hostname" ] && [ "$hstype" != "ip" ]; then
+	echo "error: parameter $4 not conforming hostname format, or given hostname is invalid"
 	exit 1
 elif [ ! -f /etc/mysql/debian.cnf ] || [ ! -f /var/run/mysqld/mysqld.pid ]; then
 	echo "error: debian-sys-maint password not found"
@@ -20,6 +25,12 @@ db=$1
 user=$2
 pass=$3
 
+if [ "$4" != "" ]; then
+	acl=$4
+else
+	acl=localhost
+fi
+
 echo "create database $db;" |mysql $access
-echo "grant all privileges on $db.* to $user@localhost identified by '$pass';" |mysql $access
+echo "grant all privileges on $db.* to $user@$acl identified by '$pass';" |mysql $access
 echo "flush privileges;" |mysql $access
