81cc6b61225843b3c571325b7aa6ef23f75c7a79
Tomasz Klim <github@tomaszklim.pl>
2017-10-29 19:24:47 +0100

    support for directadmin custom mysql

--- mysql/console.sh
+++ mysql/console.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [ ! -f /etc/mysql/debian.cnf ] || [ ! -f /var/run/mysqld/mysqld.pid ]; then
-	exit 0
+if [ -f /etc/mysql/debian.cnf ] && [ -f /var/run/mysqld/mysqld.pid ]; then
+	pass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
+	mysql -u debian-sys-maint -p$pass $@
+
+elif [ -f /usr/local/directadmin/conf/mysql.conf ]; then
+	user="`cat /usr/local/directadmin/conf/mysql.conf |grep user= |tail -n1 |sed s/user=//g`"
+	pass="`cat /usr/local/directadmin/conf/mysql.conf |grep passwd= |tail -n1 |sed s/passwd=//g`"
+	mysql -u $user -p$pass $@
 fi
-
-rootpass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
-access="-u debian-sys-maint -p$rootpass"
-
-mysql $access $@
--- mysql/list-databases.sh
+++ mysql/list-databases.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-if [ ! -f /etc/mysql/debian.cnf ] || [ ! -f /var/run/mysqld/mysqld.pid ]; then
+if [ ! -f /etc/mysql/debian.cnf ] && [ ! -f /usr/local/directadmin/conf/mysql.conf ]; then
 	exit 0
 fi
 
-rootpass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
-access="-u debian-sys-maint -p$rootpass"
+if [ -f /etc/mysql/debian.cnf ] && [ -f /var/run/mysqld/mysqld.pid ]; then
+	pass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
+	access="-u debian-sys-maint -p$pass"
+
+elif [ -f /usr/local/directadmin/conf/mysql.conf ]; then
+	user="`cat /usr/local/directadmin/conf/mysql.conf |grep user= |tail -n1 |sed s/user=//g`"
+	pass="`cat /usr/local/directadmin/conf/mysql.conf |grep passwd= |tail -n1 |sed s/passwd=//g`"
+	access="-u $user -p$pass"
+fi
 
 echo "show databases;" |mysql $access |egrep -v "^(Database|mysql|test|information_schema|performance_schema)$"
