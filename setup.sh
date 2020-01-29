f3174c2a85b106655dc04ab7df8cdc75dd35638a
Tomasz Klim <github@tomaszklim.pl>
2020-01-29 23:27:45 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@
 
 /opt/farm/scripts/setup/extension.sh sf-db-utils
 /opt/farm/scripts/setup/extension.sh sf-net-utils
-
-ln -sf /opt/farm/ext/db-tools/mysql/console.sh /usr/local/bin/sf-mysql
-ln -sf /opt/farm/ext/db-tools/postgres/console.sh /usr/local/bin/sf-pgsql
--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/sf-mysql ]; then
-	rm -f /usr/local/bin/sf-mysql
-fi
-
-if [ -h /usr/local/bin/sf-pgsql ]; then
-	rm -f /usr/local/bin/sf-pgsql
-fi
